1f86c48511dc8c1454c4a4391671a9bcd983e401
Sadiq Salau <sadiqsalau888@gmail.com>
2025-11-09 16:12:22 +0100

    feat: Apply disabled styles

--- src/renderer/src/components/NumberInput.jsx
+++ src/renderer/src/components/NumberInput.jsx
@@ -2,7 +2,13 @@ import { HiOutlineChevronUp, HiOutlineChevronDown } from "react-icons/hi2";
 import Input from "./Input";
 import { NumberInputButton } from "./NumberInputButton";
 
-export const NumberInput = ({ value, label, onChange, readOnly = true }) => (
+export const NumberInput = ({
+  value,
+  label,
+  onChange,
+  disabled,
+  readOnly = true,
+}) => (
   <div className="flex gap-2">
     <div className="flex flex-col-reverse gap-2 grow min-w-0">
       <label className="text-orange-500">{label}</label>
@@ -11,14 +17,21 @@ export const NumberInput = ({ value, label, onChange, readOnly = true }) => (
         min="1"
         value={value}
         readOnly={readOnly}
+        disabled={disabled}
         onChange={(e) => onChange(e.target.value)}
       />
     </div>
     <div className="flex flex-col gap-1 shrink-0">
-      <NumberInputButton onClick={() => onChange(parseInt(value) + 1)}>
+      <NumberInputButton
+        disabled={disabled}
+        onClick={() => onChange(parseInt(value) + 1)}
+      >
         <HiOutlineChevronUp className="size-4 stroke-3" />
       </NumberInputButton>
-      <NumberInputButton onClick={() => onChange(parseInt(value) - 1)}>
+      <NumberInputButton
+        disabled={disabled}
+        onClick={() => onChange(parseInt(value) - 1)}
+      >
         <HiOutlineChevronDown className="size-4 stroke-3" />
       </NumberInputButton>
     </div>
--- src/renderer/src/components/NumberInputButton.jsx
+++ src/renderer/src/components/NumberInputButton.jsx
@@ -7,6 +7,7 @@ export const NumberInputButton = (props) => (
       "bg-neutral-100 dark:bg-neutral-700",
       "hover:bg-orange-100 hover:text-orange-700",
       "dark:hover:bg-orange-200 dark:hover:text-orange-500",
+      "disabled:opacity-50 disabled:cursor-not-allowed",
       "flex items-center justify-center",
       "p-1 px-3 rounded-lg"
     )}
