9a923749da92bc543555f3f1aa5a883b7a964a2d
Sadiq Salau <sadiqsalau888@gmail.com>
2025-12-06 21:24:10 +0100

    feat: Set target

--- src/renderer/src/hooks/useProgress.js
+++ src/renderer/src/hooks/useProgress.js
@@ -1,16 +1,23 @@
 import { useCallback, useState } from "react";
 
 const useProgress = () => {
+  const [target, setTarget] = useState(0);
   const [progress, setProgress] = useState(0);
 
-  const resetProgress = useCallback(() => setProgress(0), []);
+  const resetProgress = useCallback(() => {
+    setTarget(0);
+    setProgress(0);
+  }, []);
+
   const incrementProgress = useCallback(
     () => setProgress((prev) => prev + 1),
     []
   );
 
   return {
+    target,
     progress,
+    setTarget,
     resetProgress,
     incrementProgress,
   };
