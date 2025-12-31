d81fcec1c1868e0d2bbfce4ba5c1e8d8af313d99
Sadiq Salau <sadiqsalau888@gmail.com>
2025-12-31 17:00:51 +0100

    feat: Updated AppUserModelId

--- src/main/index.js
+++ src/main/index.js
@@ -71,7 +71,7 @@ async function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(async () => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId("com.electron");
+  electronApp.setAppUserModelId("Purrfect Whiskers");
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
