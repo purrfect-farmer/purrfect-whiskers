6a1ca845970f51a88e4449d44cc110c708ce5aca
Sadiq Salau <sadiqsalau888@gmail.com>
2026-01-03 09:33:30 +0100

    refactor: Improved comments

--- src/renderer/src/lib/Spider.js
+++ src/renderer/src/lib/Spider.js
@@ -60,6 +60,7 @@ export default class Spider {
     return this.makeAction("wallet");
   }
 
+  /** Create Telegram Client */
   createTelegramClient(session) {
     return new TelegramClient(
       session,
