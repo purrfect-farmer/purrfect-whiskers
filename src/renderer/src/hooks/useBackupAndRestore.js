8192be33a6f2e97b2af7d1fd5009ba4c604ec804
Sadiq Salau <sadiqsalau888@gmail.com>
2026-09-24 10:48:05 +0100

    feat: Disable proxies during backup

--- src/renderer/src/hooks/useBackupAndRestore.js
+++ src/renderer/src/hooks/useBackupAndRestore.js
@@ -30,7 +30,7 @@ export default function useBackupAndRestore() {
 
         /** Configure Proxy */
         await configureProxy(partition, {
-          allowProxies,
+          allowProxies: false,
           proxyEnabled,
           proxyHost,
           proxyPort,
